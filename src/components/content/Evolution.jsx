a94778c975515414a9caf62646da1ef1e4c0ae4c
RandilT <randiltharusha72@gmail.com>
2024-02-10 12:24:29 +0530

    Create new page for evolution cards (Clash Royale)

--- src/components/content/Evolution.jsx
+++ src/components/content/Evolution.jsx
@@ -1,5 +1,56 @@
-const Evolution = () => {
-  return <div>Evolution</div>;
+import { Grid, Pagination, Stack } from "@mui/material";
+import { useEffect, useState } from "react";
+import Champion from "../Card";
+import cards from "../../data.json";
+
+const EvolutionCards = () => {
+  const evoCards = cards.items.filter(
+    (card) =>
+      card.iconUrls.evolutionMedium !== null &&
+      card.iconUrls.evolutionMedium !== undefined
+  );
+
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(11);
+  const changePage = (page) => {
+    console.log(page);
+    setStartIndex((page - 1) * 8);
+    setEndIndex(page * 8);
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {evoCards.slice(startIndex, endIndex).map((card) => {
+          return (
+            <Grid key={card.name} item xs={2} sm={4} md={4}>
+              <Champion
+                name={card.name}
+                rarity={card.rarity}
+                img={
+                  card.iconUrls.evolutionMedium
+                    ? card.iconUrls.evolutionMedium
+                    : card.iconUrls.medium
+                }
+                icon={card.iconUrls.medium}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Stack spacing={2} sx={{ float: "right" }} padding={2}>
+        <Pagination
+          count={Math.ceil(evoCards / 8)}
+          variant="outlined"
+          onChange={(e, page) => changePage(page)}
+        />
+      </Stack>
+    </>
+  );
 };
 
-export default Evolution;
+export default EvolutionCards;
